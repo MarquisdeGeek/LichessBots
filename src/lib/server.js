3a9773fd52e13d4aead5e187b967087f92ef109c
marquisdegeek <marquisdegeek@gmail.com>
2020-04-04 19:33:10 +0100

    Safely handle failed promises
    There were a couple of ,catch() cases missing. Also, I added a
    simple queue system to ensure all games had a change to be
    played, even if an exception occured inside the AI engine.

--- src/lib/server.js
+++ src/lib/server.js
@@ -38,6 +38,9 @@ module.exports = function(options) {
           setTimeout(pollServer, 10000);
         }
       })
+      .catch((err) => {
+        setTimeout(pollServer, 1000);
+      })
   }
 
   function onChallenge(data) {
@@ -52,53 +55,86 @@ module.exports = function(options) {
     engine.chatLine && engine.chatLine(data);
   }
 
+  function doGamePlay(game) {
+    return new Promise((resolve) => {
+      if (game.isMyTurn) {
+        console.log(`GAME ${game.gameId} START`);
+
+        let fen = game.fen;
+
+        // Lichess (our only current backend) does not provide a full FEN.
+        // Therefore, we fake the rest. It means our AI will never play en passant
+        // or castling, but that can be implemented by the engines themselves.
+        if (game.color === "white") {
+          fen += ' w';
+        } else {
+          fen += ' b';
+        }
+        fen += ' - - 0 1';
+
+        let currentMove;
+        engine.getMove(game.gameId, game.color, fen)
+        .then((move) => {
+          currentMove = move;
+          return backend.sendMove(game.gameId, currentMove);
+        })
+        .then((res) => {
+          if (res.error) {
+            // Usually 'piece cannot move' because the engine did something stupid.
+            backend.sendMessage(game.gameId, `Sorry - I'm not thinking straight`);
+            console.log(`GAME ${game.gameId} BOTERROR ${currentMove}`);
+            return getRandomMove(game.gameId, game.color, fen);
+          }
+          return undefined;
+        })
+        .then((rndMove) => {
+          if (rndMove) { // we trust that the random move is valid
+            currentMove = rndMove;
+            console.log(`GAME ${game.gameId} BOTRANDOM ${currentMove}`);
+            return backend.sendMove(game.gameId, currentMove);
+          }
+        })
+        .then(() => {
+          console.log(`GAME ${game.gameId} MOVES ${currentMove} FROM ${fen}`);
+          resolve(0); // still playing
+        })
+        .catch((err) => {
+          console.log(`GAME ${game.gameId} BOTERR ${err}`);
+          resolve(0);
+        });
+      } else {
+        resolve(1);
+      }
+    }); //promise
+  }
+
   function doProcessingTick() {
     return new Promise((resolve) => {
       let gamesLeft = 0;
       let gameWaiting = 0;
-      // TODO: queue all moves, so resolve only starts again once everyone has moved
+      let promiseList = [];
 
       backend.getGameList()
         .then(json => {
           let nowPlaying = json.nowPlaying;
           gamesLeft += nowPlaying.length;
+
           //console.log(json)
           nowPlaying.forEach((game) => {
-            if (game.isMyTurn) {
-              let fen = game.fen;
-
-              // Lichess (our only current backend) does not provide a full FEN.
-              // Therefore, we fake the rest. It means our AI will never play en passant
-              // or castling, but that can be implemented by the engines themselves.
-              if (game.color === "white") {
-                fen += ' w';
-              } else {
-                fen += ' b';
-              }
-              fen += ' - - 0 1';
-
-              let currentMove;
-              engine.getMove(game.gameId, game.color, fen)
-              .then((move) => {
-                currentMove = move;
-                return backend.sendMove(game.gameId, move);
-              })
-              .then((res) => {
-                if (res.error) {
-                  // Usually 'piece cannot move' because the engine did something stupid.
-                  backend.sendMessage(game.gameId, `Sorry - I'm not thinking straight`);
-                  return getRandomMove(game.gameId, game.color, fen);
-                }
-              })
-              .then((rndMove) => {
-                console.log(`PLAY ${game.gameId} MOVES ${currentMove || rndMove} FROM ${fen} ${rndMove?'BOT error':''}`);
-              });
-            } else {
-              ++gameWaiting;
-            }
+            let prom = doGamePlay(game);
+            prom
+            .then((stillPlaying) => {
+              gameWaiting += stillPlaying;
+            })
+            .catch((err) => {
+              console.log(`ERR goGamePlay ${err.message}`);
+            });
+            promiseList.push(prom);
           });
 
           // TODO: post this to a 'bots stats page'
+
+          return Promise.all(promiseList);
         })
         .then(() => {
           if (--statsMessageCount < 0) {
@@ -107,6 +143,10 @@ module.exports = function(options) {
           }
           resolve(gamesLeft)
         })
+        .catch((err) => {
+          console.log(`ENGINE getGameList failed ${err}`);
+          throw new Error(`Could not retrieve games list`);
+        });
     });
   }
 
